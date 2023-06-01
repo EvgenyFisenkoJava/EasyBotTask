6491418ab620c2b177a83586bd90005183dfb026
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-06-01 14:21:06 +0200

    Initial commit

---
+++ src/main/java/com/example/easybottask/EasyBotTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.easybottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBotTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBotTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easybottask/EasyBotTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easybottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBotTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
